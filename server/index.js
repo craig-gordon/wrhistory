0d2120fc6f34f24fb962d4f34f7530d82db212b9
Craig Gordon <cyghfer@gmail.com>
2018-02-20 14:32:09 -0500

    Add .env file + edit server index file

--- server/index.js
+++ server/index.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 
 const server = require('./routes.js');
 
-server.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
+server.listen(process.env.PORT, () => console.log(`WR History server listening on port ${process.env.PORT}`));
