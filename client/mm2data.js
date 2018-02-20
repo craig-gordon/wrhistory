c97fa3b60e26410c6a627fb5e8a3aa54d24b1bde
Craig Gordon <cyghfer@gmail.com>
2018-02-20 17:44:26 -0500

    Add sample data file

---
+++ client/mm2data.js
@@ -0,0 +1,175 @@
+export const entry1 = {
+    time: {
+      hours: 0,
+      minutes: 26,
+      seconds: 42
+    },
+    runner: 'cyghfer',
+    platform: 'Nintendo Entertainment System',
+    version: 'J',
+    date: {
+      year: 2018,
+      month: 2,
+      day: 6
+    }
+  };
+  
+  export const entry2 = {
+    time: {
+      hours: 0,
+      minutes: 26,
+      seconds: 48
+    },
+    runner: 'Ellonija',
+    platform: 'Nintendo Entertainment System (Flash Cart)',
+    version: 'J',
+    date: {
+      year: 2016,
+      month: 1,
+      day: 29
+    }
+  };
+  
+  export const entry3 = {
+    time: {
+      hours: 0,
+      minutes: 26,
+      seconds: 55
+    },
+    runner: 'Ellonija',
+    platform: 'Nintendo Entertainment System (Flash Cart)',
+    version: 'J',
+    date: {
+      year: 2015,
+      month: 10,
+      day: 22
+    }
+  };
+  
+  export const entry4 = {
+    time: {
+      hours: 0,
+      minutes: 26,
+      seconds: 56
+    },
+    runner: 'Ellonija',
+    platform: 'Nintendo Entertainment System (Flash Cart)',
+    version: 'J',
+    date: {
+      year: 2015,
+      month: 10,
+      day: 21
+    }
+  };
+  
+  export const entry5 = {
+    time: {
+      hours: 0,
+      minutes: 26,
+      seconds: 57
+    },
+    runner: 'Ellonija',
+    platform: 'Nintendo Entertainment System (Flash Cart)',
+    version: 'J',
+    date: {
+      year: 2014,
+      month: 11,
+      day: 13
+    }
+  };
+  
+  export const entry6 = {
+    time: {
+      hours: 0,
+      minutes: 27,
+      seconds: 1
+    },
+    runner: 'cyghfer',
+    platform: 'Nintendo Entertainment System',
+    version: 'J',
+    date: {
+      year: 2014,
+      month: 7,
+      day: 20
+    }
+  };
+  
+  export const entry7 = {
+    time: {
+      hours: 0,
+      minutes: 27,
+      seconds: 8
+    },
+    runner: 'shoka',
+    platform: 'Emulator',
+    version: 'J',
+    date: {
+      year: 2013,
+      month: 12,
+      day: 8
+    }
+  };
+  
+  export const entry8 = {
+    time: {
+      hours: 0,
+      minutes: 27,
+      seconds: 17
+    },
+    runner: 'cyghfer',
+    platform: 'Nintendo Entertainment System',
+    version: 'J',
+    date: {
+      year: 2013,
+      month: 2,
+      day: 6
+    }
+  };
+  
+  export const entry9 = {
+    time: {
+      hours: 0,
+      minutes: 27,
+      seconds: 21
+    },
+    runner: 'nou1',
+    platform: 'Nintendo Entertainment System',
+    version: 'J',
+    date: {
+      year: 2011,
+      month: 3,
+      day: 26
+    }
+  };
+  
+  export const entry10 = {
+    time: {
+      hours: 0,
+      minutes: 27,
+      seconds: 51
+    },
+    runner: 'Seth Glass',
+    platform: 'Nintendo Entertainment System',
+    version: 'U',
+    date: {
+      year: 2010,
+      month: 4,
+      day: 26
+    }
+  };
+  
+  export const entry11 = {
+    time: {
+      hours: 0,
+      minutes: 30,
+      seconds: 39
+    },
+    runner: 'Richard Ureta',
+    platform: 'Nintendo Entertainment System',
+    version: 'U',
+    date: {
+      year: 2004,
+      month: 7,
+      day: 30
+    }
+  };
