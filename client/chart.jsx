4aaeab4dd9324022a2d602a8289f3f9fd0db0299
Craig Gordon <cyghfer@gmail.com>
2018-03-09 15:59:53 -0500

    Add x-axis information for both the chart and data

--- client/chart.jsx
+++ client/chart.jsx
@@ -12,6 +12,15 @@ class Chart extends React.Component {
       title: {
         text: 'Mega Man 2 Any% World Record History'
       },
+      xAxis: {
+        title: {
+          text: 'Date'
+        },
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          year: '%Y'
+        }
+      },
       yAxis: {
         title: {
           text: 'Time'
@@ -19,24 +28,26 @@ class Chart extends React.Component {
       },
       plotOptions: {
         series: {
-          pointStart: 2004
+          pointStart: Date.UTC(2000),
+          pointInterval: 365 * 24 * 3600 * 1000
         }
       },
       series: [{
-        name: 
         data: [
-          1839,
-          1671,
-          1641,
-          1637,
-          1628,
-          1621,
-          1617,
-          1616,
-          1615,
-          1608,
-          1602,
-        ]
+          [Date.UTC(2000, 0, 1), 1839],
+          [Date.UTC(2005, 0, 1), 1671],
+          [Date.UTC(2008, 0, 1), 1641],
+          [Date.UTC(2009, 0, 1), 1637],
+          [Date.UTC(2010, 0, 1), 1628],
+          [Date.UTC(2013, 0, 1), 1621],
+          [Date.UTC(2014, 0, 1), 1617],
+          [Date.UTC(2015, 0, 1), 1616],
+          [Date.UTC(2016, 0, 1), 1615],
+          [Date.UTC(2017, 0, 1), 1608],
+          [Date.UTC(2018, 0, 1), 1602],
+        ],
+        pointStart: Date.UTC(2000),
+        pointInterval: 365 * 24 * 3600 * 1000
       }]
     }
   }
