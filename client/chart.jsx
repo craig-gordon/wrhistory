e01defeca059135f79e6585594a7611e56ea3e2e
Craig Gordon <cyghfer@gmail.com>
2018-03-14 17:27:44 -0400

    Configure Chart component to make use of annotations

--- client/chart.jsx
+++ client/chart.jsx
@@ -1,133 +1,151 @@
 import React from 'react';
+import Highcharts from 'highcharts';
+import Annotations from 'highcharts/modules/annotations';
 import ReactHighcharts from 'react-highcharts';
 import { secsToTs } from './timeConversions.js';
 import data from './mm2data.js';
 
+Annotations(ReactHighcharts.Highcharts);
+
 class Chart extends React.Component {
   constructor(props) {
     super(props);
-    this.config = {
-      chart: {
-        type: 'line'
-      },
-      title: {
-        text: 'Mega Man 2 Any% World Record History'
-      },
-      credits: false,
-      plotOptions: {
-        line: {
-          marker: {
-            enabled: true
-          }
-        }
-      },
-      xAxis: {
-        title: {
-          text: 'Date'
+    this.state = {
+      config: {
+        chart: {
+          type: 'line',
+          zoomType: 'x',
+          panning: true
         },
-        type: 'datetime',
-        min: Date.UTC(2004, 0, 1),
-        dateTimeLabelFormats: {
-          year: '%Y'
-        }
-      },
-      yAxis: {
         title: {
-          text: 'Time'
+          text: 'Mega Man 2 Any% World Record History'
         },
-        type: 'datetime',
-        dateTimeLabelFormats: {
-          milliseconds: '%H:%M:%S',
-          second: '%H:%M:%S',
-          minute: '%M:%S',
-          hour: '%H:%M:%S'
-        }
-      },
-      tooltip: {
-        formatter: function() {
-          console.log('this:', this);
-          return (
-            `
-              <div>
-                <div>Time: ${secsToTs(this.y / 1000)}</div><br/>
-                <div>Player: ${this.point.player}</div><br/>
-                <div>Date: ${(new Date(this.x)).toDateString().slice(4)}</div><br/>
-                ${this.point.note ? '<div>Note: ' + this.point.note + '</div><br/>' : ''}
-              </div>
-            `
-          );
-        }
-      },
-      series: [{
-        step: 'left',
-        showInLegend: false,
-        cursor: 'pointer',
-        data: [
-          {
-            x: Date.UTC(data[0].year, data[0].month, data[0].day),
-            y: data[0].time * 1000,
-            player: data[0].player
-          },
-          {
-            x: Date.UTC(data[1].year, data[1].month, data[1].day),
-            y: data[1].time * 1000,
-            player: data[1].player
-          },
-          {
-            x: Date.UTC(data[2].year, data[2].month, data[2].day),
-            y: data[2].time * 1000,
-            player: data[2].player
-          },
-          {
-            x: Date.UTC(data[3].year, data[3].month, data[3].day),
-            y: data[3].time * 1000,
-            player: data[3].player
-          },
-          {
-            x: Date.UTC(data[4].year, data[4].month, data[4].day),
-            y: data[4].time * 1000,
-            player: data[4].player,
-            color: '#FF0000',
-            note: 'This run was performed with 10hz turbo, though it was still recognized as the legitimate record.'
-          },
-          {
-            x: Date.UTC(data[5].year, data[5].month, data[5].day),
-            y: data[5].time * 1000,
-            player: data[5].player
-          },
-          {
-            x: Date.UTC(data[6].year, data[6].month, data[6].day),
-            y: data[6].time * 1000,
-            player: data[6].player
-          },
-          {
-            x: Date.UTC(data[7].year, data[7].month, data[7].day),
-            y: data[7].time * 1000,
-            player: data[7].player
-          },
-          {
-            x: Date.UTC(data[8].year, data[8].month, data[8].day),
-            y: data[8].time * 1000,
-            player: data[8].player
-          },
-          {
-            x: Date.UTC(data[9].year, data[9].month, data[9].day),
-            y: data[9].time * 1000,
-            player: data[9].player
-          },
-          {
-            x: Date.UTC(data[10].year, data[10].month, data[10].day),
-            y: data[10].time * 1000,
-            player: data[10].player,
-            color: '#00FF00'
+        credits: false,
+        plotOptions: {
+          line: {
+            marker: {
+              enabled: true
+            }
           }
-        ]
-      }]
+        },
+        xAxis: {
+          title: {
+            text: 'Date'
+          },
+          type: 'datetime',
+          min: Date.UTC(2004, 0, 1),
+          dateTimeLabelFormats: {
+            year: '%Y'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Time'
+          },
+          type: 'datetime',
+          dateTimeLabelFormats: {
+            milliseconds: '%H:%M:%S',
+            second: '%H:%M:%S',
+            minute: '%M:%S',
+            hour: '%H:%M:%S'
+          }
+        },
+        tooltip: {
+          formatter: function() {
+            return (
+              `
+                <div>
+                  <div>Time: ${secsToTs(this.y / 1000)}</div><br/>
+                  <div>Player: ${this.point.player}</div><br/>
+                  <div>Date: ${(new Date(this.x)).toDateString().slice(4)}</div><br/>
+                  ${this.point.note ? '<div>Note: ' + this.point.note + '</div><br/>' : ''}
+                </div>
+              `
+            );
+          }
+        },
+        annotations: [{
+          labels: [{
+              point: {
+                  xAxis: 0,
+                  yAxis: 1,
+                  x: 1517943808450,
+                  y: 3.4162381858686803
+              },
+              text: 'LABEL'
+          }]
+        }],
+        series: [{
+          step: 'left',
+          showInLegend: false,
+          cursor: 'pointer',
+          data: [
+            {
+              x: Date.UTC(data[0].year, data[0].month, data[0].day),
+              y: data[0].time * 1000,
+              player: data[0].player
+            },
+            {
+              x: Date.UTC(data[1].year, data[1].month, data[1].day),
+              y: data[1].time * 1000,
+              player: data[1].player
+            },
+            {
+              x: Date.UTC(data[2].year, data[2].month, data[2].day),
+              y: data[2].time * 1000,
+              player: data[2].player
+            },
+            {
+              x: Date.UTC(data[3].year, data[3].month, data[3].day),
+              y: data[3].time * 1000,
+              player: data[3].player
+            },
+            {
+              x: Date.UTC(data[4].year, data[4].month, data[4].day),
+              y: data[4].time * 1000,
+              player: data[4].player,
+              color: '#FF0000',
+              note: 'This run was performed with 10hz turbo, though it was still recognized as the legitimate record.'
+            },
+            {
+              x: Date.UTC(data[5].year, data[5].month, data[5].day),
+              y: data[5].time * 1000,
+              player: data[5].player
+            },
+            {
+              x: Date.UTC(data[6].year, data[6].month, data[6].day),
+              y: data[6].time * 1000,
+              player: data[6].player
+            },
+            {
+              x: Date.UTC(data[7].year, data[7].month, data[7].day),
+              y: data[7].time * 1000,
+              player: data[7].player
+            },
+            {
+              x: Date.UTC(data[8].year, data[8].month, data[8].day),
+              y: data[8].time * 1000,
+              player: data[8].player
+            },
+            {
+              x: Date.UTC(data[9].year, data[9].month, data[9].day),
+              y: data[9].time * 1000,
+              player: data[9].player
+            },
+            {
+              x: Date.UTC(data[10].year, data[10].month, data[10].day),
+              y: data[10].time * 1000,
+              player: data[10].player,
+              color: '#00FF00'
+            }
+          ]
+        }]
+      }
     }
   }
 
   render() {
-    return <ReactHighcharts config={this.config} ref="chart"></ReactHighcharts>
+    return <ReactHighcharts config={this.state.config} ref="chart" />
   }
 }
 
