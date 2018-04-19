b95fb8e5742cab5e341c89532747852e866ebb70
Craig Gordon <cyghfer@gmail.com>
2018-04-19 17:25:37 -0400

    Add formula for "duration as WR" to tooltip

--- client/chart.jsx
+++ client/chart.jsx
@@ -102,12 +102,18 @@ class Chart extends React.Component {
         },
         tooltip: {
           formatter: function() {
+            console.log('tooltip this:', this);
             return (
               `
                 <div>
                   <div>Time: ${secsToTs(this.y / 1000)}</div><br/>
                   <div>Player: ${this.point.player}</div><br/>
                   <div>Date: ${(new Date(this.x)).toDateString().slice(4)}</div><br/>
+                  <div>Duration as WR: ${(Date.UTC(
+                    data[this.point.index + 1].year,
+                    data[this.point.index + 1].month,
+                    data[this.point.index + 1].day
+                  ) - this.x) / 86400000} Days</div><br/>
                   ${this.point.note ? '<div>Note: ' + this.point.note + '</div><br/>' : ''}
                 </div>
               `
