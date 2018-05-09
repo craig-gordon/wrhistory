1414e949889b091d80801200dc416529535714ec
Craig Gordon <cyghfer@gmail.com>
2018-05-09 18:10:43 -0400

    Add basic styling through injectGlobal

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @font-face {
+    font-family: 'Varela Round', sans-serif;
+    src: url('https://fonts.googleapis.com/css?family=Varela+Round');
+  }
+  body {
+    font-family: 'Varela Round', sans-serif;
+    margin: 5% 8%;
+    background: 'azure';
+  }
+`;
 
 render(
   <BrowserRouter>
